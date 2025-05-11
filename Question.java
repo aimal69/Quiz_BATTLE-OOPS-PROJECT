d3f7bf586c51c8ede31acea91230eb267b9e0047
Aimal jan <aimalsami753@gmail.com>
2025-05-11 12:11:31 +0000

    done

---
+++ Question.java
@@ -0,0 +1,13 @@
+public class Question {
+    public String question, optionA, optionB, optionC, optionD;
+    public char correctOption;
+
+    public Question(String question, String a, String b, String c, String d, char correct) {
+        this.question = question;
+        this.optionA = a;
+        this.optionB = b;
+        this.optionC = c;
+        this.optionD = d;
+        this.correctOption = correct;
+    }
+}
